0df4a518c70b2ba43368e19c168c7d710568bb9c
Inan Evin <inanevin@gmail.com>
2019-04-19 23:02:22 +0300

    Scene: Scene setup

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,19 +1,81 @@
 package sample;
 
 import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Rectangle2D;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+
+import javafx.scene.control.ToolBar;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.control.Button;
+
+import javafx.scene.layout.HBox;
+
+class WindowButtons extends HBox {
+
+    public WindowButtons() {
+        Button closeBtn = new Button("X");
+
+        closeBtn.setOnAction(new EventHandler<ActionEvent>() {
+
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                Platform.exit();
+            }
+        });
+
+        this.getChildren().add(closeBtn);
+    }
+}
 
 public class Main extends Application {
 
+    private double xOffset = 0;
+    private double yOffset = 0;
+
+
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
-        primaryStage.show();
+            Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+
+            root.setOnMousePressed(new EventHandler<MouseEvent>() {
+                @Override
+                public void handle(MouseEvent event) {
+                    xOffset = event.getSceneX();
+                    yOffset = event.getSceneY();
+                }
+            });
+
+             root.setOnMouseDragged(new EventHandler<MouseEvent>() {
+                @Override
+                public void handle(MouseEvent event) {
+                primaryStage.setX(event.getScreenX() - xOffset);
+                primaryStage.setY(event.getScreenY() - yOffset);
+                }
+             });
+
+             Scene scene = new Scene(root, 1024, 576);
+             scene.setFill(Color.TRANSPARENT);
+            primaryStage.setTitle("Lina Engine Build Launcher");
+            primaryStage.setScene(scene);
+            primaryStage.setResizable(false);
+            primaryStage.initStyle(StageStyle.TRANSPARENT);
+            //primaryStage.getScene().getStylesheets().setAll(Main.class.getResource("main.css").toString());
+            primaryStage.show();
+
+
     }
 
 
