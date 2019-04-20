27d7c3c476af4e39e55b783d0144a6bdf3c67c29
Inan Evin <inanevin@gmail.com>
2019-04-20 20:52:45 +0300

    table view & other ui

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -2,14 +2,15 @@ package sample;
 
 import javafx.application.Application;
 import javafx.application.Platform;
+import javafx.collections.FXCollections;
 import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.geometry.Rectangle2D;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 
-import javafx.scene.control.Hyperlink;
-import javafx.scene.control.ToolBar;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.BorderPane;
@@ -21,45 +22,26 @@ import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
-import javafx.scene.control.Button;
 
 import javafx.scene.layout.HBox;
 
+import javax.xml.soap.Text;
 import java.util.Timer;
 import java.util.TimerTask;
 
-class WindowButtons extends HBox {
 
-    public WindowButtons() {
-        Button closeBtn = new Button("X");
-
-        closeBtn.setOnAction(new EventHandler<ActionEvent>() {
-
-            @Override
-            public void handle(ActionEvent actionEvent) {
-                Platform.exit();
-            }
-        });
-
-        this.getChildren().add(closeBtn);
-    }
-}
-
-class EnableDynamicLogo extends TimerTask
-{
-    EnableDynamicLogo(ImageView staticView, ImageView dynamicView, Timer target)
-    {
+class EnableDynamicLogo extends TimerTask {
+    EnableDynamicLogo(ImageView staticView, ImageView dynamicView, Timer target) {
         staticImage = staticView;
         dynamicImage = dynamicView;
         targetTimer = target;
     }
 
     @Override
-    public void run()
-    {
+    public void run() {
         dynamicImage.setVisible(true);
         staticImage.setVisible(false);
-        targetTimer.schedule(new DisableDynamicLogo(staticImage, dynamicImage), 2*1000);
+        targetTimer.schedule(new DisableDynamicLogo(staticImage, dynamicImage), 2 * 1000);
     }
 
     private ImageView staticImage;
@@ -67,17 +49,14 @@ class EnableDynamicLogo extends TimerTask
     Timer targetTimer;
 }
 
-class DisableDynamicLogo extends TimerTask
-{
-    DisableDynamicLogo(ImageView staticView, ImageView dynamicView)
-    {
+class DisableDynamicLogo extends TimerTask {
+    DisableDynamicLogo(ImageView staticView, ImageView dynamicView) {
         staticImage = staticView;
         dynamicImage = dynamicView;
     }
 
     @Override
-    public void run()
-    {
+    public void run() {
         dynamicImage.setVisible(false);
         staticImage.setVisible(true);
     }
@@ -87,6 +66,26 @@ class DisableDynamicLogo extends TimerTask
 
 }
 
+class TableRowBase
+{
+    TableRowBase(String id) { m_ID = id; }
+
+    public String GetID(){ return m_ID; }
+
+    private String m_ID;
+}
+class TableRow<T> extends TableRowBase
+{
+    TableRow(String id, T data) { super(id); m_Data = data;}
+
+    public T GetData() { return m_Data; }
+    public void SetData(T data) { m_Data = data;}
+
+    private T m_Data;
+}
+
+
+
 public class Main extends Application {
 
     private double xOffset = 0;
@@ -95,6 +94,62 @@ public class Main extends Application {
     ImageView logoStatic;
     Timer logoTimer;
     private int logoChangeRate = 5;
+    TextField sourceField, buildField;
+    ComboBox<String> generatorBox;
+    TableView optionsTable;
+
+    String generators[] =
+            {
+                    "Visual Studio 15 2017",
+                    "Visual Studio 15 2017 ARM",
+                    "Visual Studio 15 2017 Win64",
+                    "Visual Studio 14 2015",
+                    "Visual Studio 14 2015 ARM",
+                    "Visual Studio 14 2015 Win64",
+                    "Visual Studio 12 2013",
+                    "Visual Studio 12 2013 ARM",
+                    "Visual Studio 12 2013 Win64",
+                    "Visual Studio 11 2012",
+                    "Visual Studio 11 2012 ARM",
+                    "Visual Studio 11 2012 Win64",
+                    "Visual Studio 10 2010",
+                    "Visual Studio 10 2010 ARM",
+                    "Visual Studio 10 2010 IA64",
+                    "Visual Studio 9 2008",
+                    "Visual Studio 9 2008 ARM",
+                    "Visual Studio 9 2008 IA64",
+                    "Borland Makefiles",
+                    "NMake Makefiles",
+                    "NMake Makefiles JOM",
+                    "Green Hills MULTI",
+                    "MSYS Makefiles",
+                    "MinGW Makefiles",
+                    "Unix Makefiles",
+                    "Ninja",
+                    "Watcom WMake",
+                    "CodeBlocks - MinGW Makefiles",
+                    "CodeBlocks - NMake Makefiles",
+                    "CodeBlocks - NMake Makefiles JOM",
+                    "CodeBlocks - Ninja",
+                    "CodeBlocks - Unix Makefiles",
+                    "CodeLite - MinGW Makefiles",
+                    "CodeLite - NMake Makefiles",
+                    "CodeLite - Ninja",
+                    "CodeLite - Unix Makefiles",
+                    "Sublime Text 2 - MinGW Makefiles",
+                    "Sublime Text 2 - NMake Makefiles",
+                    "Sublime Text 2 - Ninja",
+                    "Sublime Text 2 - Unix Makefiles",
+                    "Kate - MinGW Makefiles",
+                    "Kate - NMake Makefiles",
+                    "Kate - Ninja",
+                    "Kate - Unix Makefiles",
+                    "Eclipse CDT4 - NMake Makefiles",
+                    "Eclipse CDT4 - MinGW Makefiles",
+                    "Eclipse CDT4 - Ninja",
+                    "Eclipse CDT4 - Unix Makefiles"
+
+            };
 
     @Override
     public void start(Stage primaryStage) throws Exception {
@@ -124,8 +179,14 @@ public class Main extends Application {
         scene.setFill(Color.TRANSPARENT);
         Hyperlink hyperLink = (Hyperlink) scene.lookup("#hyperlink");
 
-        logoDynamic = (ImageView)scene.lookup("#logoDynamic");
-        logoStatic = (ImageView)scene.lookup("#logoStatic");
+        logoDynamic = (ImageView) scene.lookup("#logoDynamic");
+        logoStatic = (ImageView) scene.lookup("#logoStatic");
+        generatorBox = (ComboBox<String>) scene.lookup("#generatorComboBox");
+        optionsTable = (TableView) scene.lookup("#optionsTable");
+        sourceField = (TextField) scene.lookup("#sourceField");
+        buildField = (TextField) scene.lookup("#buildField");
+
+        generatorBox.setItems(FXCollections.observableArrayList(generators));
 
         hyperLink.setOnAction(new EventHandler<ActionEvent>() {
             @Override
@@ -133,6 +194,25 @@ public class Main extends Application {
                 getHostServices().showDocument("https://github.com/inanevin/LinaEngine");
             }
         });
+
+
+
+
+        TableColumn<String, TableRowBase> column1 = new TableColumn("Option");
+        column1.setCellValueFactory(new PropertyValueFactory<>("Option"));
+
+
+        TableColumn<String, TableRowBase> column2 = new TableColumn("Value");
+        column1.setCellValueFactory(new PropertyValueFactory<>("Value"));
+
+        optionsTable.getItems().add(new TableRow<Boolean>("BUILD_SHARED_LIBS", false));
+        optionsTable.getItems().add(new TableRow<String>("CMAKE_CONFIGURATION_TYPES", "Debug;Release;MinSizeRel;RelWithDebInfo;"));
+        optionsTable.getItems().add(new TableRow<Boolean>("LINA_BUILD_SANDBOX", true));
+        optionsTable.getItems().add(new TableRow<Boolean>("LINA_ENABLE_LOGGING", true));
+
+
+
+
         primaryStage.setTitle("Lina Engine Build Launcher");
         primaryStage.setScene(scene);
         primaryStage.setResizable(false);
@@ -140,7 +220,7 @@ public class Main extends Application {
         //primaryStage.getScene().getStylesheets().setAll(Main.class.getResource("main.css").toString());
         primaryStage.show();
 
-        logoTimer.scheduleAtFixedRate(new EnableDynamicLogo(logoStatic, logoDynamic, logoTimer), logoChangeRate*1000, logoChangeRate*1000);
+        logoTimer.scheduleAtFixedRate(new EnableDynamicLogo(logoStatic, logoDynamic, logoTimer), logoChangeRate * 1000, logoChangeRate * 1000);
     }
 
 
@@ -148,3 +228,4 @@ public class Main extends Application {
         launch(args);
     }
 }
+
